6f9ed98f6ffe900d82a88bf7c059a73f07030c42
abc-112233 <dewarabadulla@gmail.com>
2025-07-29 16:33:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~JdxAjLzQ#J0LeGGHKRFcSGCqCwduAVINGqgeMCHngafd3rxEyong'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?XpNB8J'
 };
